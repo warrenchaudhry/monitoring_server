8d48a27b13dae361dfb069610134a9bf441cd3ee
Warren Chaudhry <warrenchaudhry@gmail.com>
2016-10-01 02:30:28 +0800

    assign redis url in production

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,1 +1,6 @@
-$redis = Redis::Namespace.new("monitoring_server", :redis => Redis.new)
+if Rails.env.production?
+  redis = Redis.new(url: ENV['REDISTOGO_URL'])
+else
+  redis = Redis.new
+end
+$redis = Redis::Namespace.new("monitoring_server", redis: redis)
